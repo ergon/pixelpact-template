b5425ff82ddb9884d98b5a5042bee9beeb391491
Raphael Zimmermann <raphael.zimmermann@ergon.ch>
2024-08-20 09:57:01 +0200

    Use playwright browsers from nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,16 +2,27 @@
   description = "Pixelpact Project Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    playwright.url = "github:pietdevries94/playwright-web-flake/1.46.1";
   };
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    playwright,
+    nixpkgs,
+    ...
+  }: let
     forAllSystems = function:
       nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux"]
       (system:
-        function (import nixpkgs {
-          inherit system;
-        }));
+        function (let
+          overlay = final: prev: {
+            inherit (playwright.packages.${system}) playwright-test playwright-driver;
+          };
+        in
+          import nixpkgs {
+            inherit system;
+            overlays = [overlay];
+          }));
   in {
     devShells = forAllSystems (
       pkgs: {
@@ -19,6 +30,8 @@
           buildInputs = with pkgs; [nodejs];
           shellHook = ''
             export REPOSITORY_ROOT=$(pwd)
+            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+            export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS
           '';
         };
       }
